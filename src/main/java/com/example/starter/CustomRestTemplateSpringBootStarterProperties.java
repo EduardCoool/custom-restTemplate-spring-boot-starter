93b94e918bf186c1474ab62d1854434966275ffe
79510 <eduard-proskurin@mail.ru>
2023-09-28 22:56:20 +0300

    -

--- src/main/java/com/example/starter/CustomRestTemplateSpringBootStarterProperties.java
+++ src/main/java/com/example/starter/CustomRestTemplateSpringBootStarterProperties.java
@@ -1,16 +1,9 @@
-package com.example.customresttemplatespringbootstarter;
+package com.example.starter;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "custom-rest-template-spring-boot-starter")
 public class CustomRestTemplateSpringBootStarterProperties {
 
-    private String userName;
-    private String morningMessage;
-    private String afternoonMessage;
-    private String eveningMessage;
-    private String nightMessage;
-
-    // standard getters and setters
 
 }
